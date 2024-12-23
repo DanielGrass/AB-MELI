88458e5ac14a880a85719ed1625228a18bcbedd4
DanielGrass <dsgrassg@unal.edu.co>
2024-12-23 07:41:54 -0500

    ETL Mejoras

--- models/ETL.py
+++ models/ETL.py
@@ -1,6 +1,6 @@
 from pyspark.sql.types import IntegerType
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import regexp_replace, split, explode, col, when, lit, first, row_number, sum as F_sum, max as F_max, last, countDistinct, to_date
+from pyspark.sql.functions import regexp_replace, split, explode, col, when, lit, row_number, sum as F_sum, last, countDistinct, to_date
 from pyspark.sql import Window
 
 spark = SparkSession.builder \
@@ -28,13 +28,22 @@ s3_path = f"s3a://{bucket_name}/{file_key}"
 delta_table_bronze_path = f"s3a://{bucket_name}/delta-table-bronze/"
 
 
-# # Leer el archivo CSV desde S3
-# df = spark.read.csv(s3_path, header=True, inferSchema=True)
+# Leer el archivo CSV desde S3
+df = spark.read.csv(s3_path, header=True, inferSchema=True)
 
-# # Guardar como Delta Table
-# df.orderBy("user_id", "timestamp").write.format("delta").mode("overwrite").save(delta_table_bronze_path)
+# Guardar como Delta Table
+df.orderBy("user_id", "timestamp").write.format("delta").mode("overwrite").save(delta_table_bronze_path)
 
-# print(f"Delta Table guardada en: {delta_table_bronze_path}")
+columns_to_check = ["user_id", "event_name", "timestamp", "experiments"]
+df_nulls = df.select(
+    *[F_sum(col(column).isNull().cast("int")).alias(column) for column in columns_to_check]
+)
+
+print("Valores nulos por columna:")
+df_nulls.show()
+
+
+print(f"Delta Table guardada en: {delta_table_bronze_path}")
 
 
 ########################################
@@ -44,66 +53,66 @@ delta_table_bronze_path = f"s3a://{bucket_name}/delta-table-bronze/"
 # Ruta donde se guardará la tabla Delta
 delta_table_silver_path = f"s3a://{bucket_name}/delta-table-silver-v1/"
 
-# # Cargar la tabla Delta Bronze
-# df_delta = spark.read.format("delta").load(delta_table_bronze_path)
+# Cargar la tabla Delta Bronze
+df_delta = spark.read.format("delta").load(delta_table_bronze_path)
 
-# # # PARTE I: Marcar eventos relacionados con compras
+# # PARTE I: Marcar eventos relacionados con compras
 
-# # Marcar eventos de compra
-# df_delta = df_delta.withColumn(
-#     "is_buy", 
-#     when(col("event_name") == "BUY", lit(1)).otherwise(lit(0))
-# )
+# Marcar eventos de compra
+df_delta = df_delta.withColumn(
+    "is_buy", 
+    when(col("event_name") == "BUY", lit(1)).otherwise(lit(0))
+)
 
-# # Crear una ventana para propagar el grupo de compras hacia atrás (orden descendente)
-# user_window_desc = Window.partitionBy("user_id").orderBy(col("timestamp").desc())
+# Crear una ventana para propagar el grupo de compras hacia atrás (orden descendente)
+user_window_desc = Window.partitionBy("user_id").orderBy(col("timestamp").desc())
 
-# # Propagar los grupos de compras hacia atrás
-# df_delta = df_delta.withColumn(
-#     "buy_group",
-#     last(when(col("is_buy") == 1, row_number().over(user_window_desc)), ignorenulls=True).over(user_window_desc)
-# )
-
-
-# # Asignar grupo 0 a eventos no relacionados con compras
-# df_delta = df_delta.withColumn(
-#     "flag_purchase",
-#     when(col("buy_group").isNull(), lit(False)).otherwise(lit(True))
-# )
-
-# df_delta = df_delta.orderBy("user_id", "timestamp")
-
-# # Mostrar resultados
-# df_delta.select("user_id", "timestamp", "event_name", "buy_group", "flag_purchase").filter(col("user_id") == 1683).show(n=1000,truncate=False) #Usuario con usa sola sesion y con compra
-# df_delta.select("user_id", "timestamp", "event_name", "buy_group", "flag_purchase").filter(col("user_id") == 847572).show(n=1000,truncate=False) #Usuario con varias compras
-# df_delta.select("user_id", "timestamp", "event_name", "buy_group", "flag_purchase").filter(col("user_id") == 1876).show(n=1000,truncate=False) #Usuario con varias sesiones pero sin compras
-
-# # PARTE II: Explosión de datos después de marcar
-# # Limpiar la columna 'experiments' para quitar las llaves '{' y '}'
-# df_cleaned = df_delta.withColumn("experiments", regexp_replace(col("experiments"), "[{}]", ""))
-
-# # Separar los experimentos en un array
-# df_array = df_cleaned.withColumn("experiments_array", split(col("experiments"), ", "))
-
-# # Explode para separar cada experimento en filas individuales
-# df_exploded = df_array.withColumn("experiment", explode(col("experiments_array")))
-
-# # Separar experiment_name y variant_id
-# df_final = df_exploded.withColumn("experiment_name", split(col("experiment"), "=")[0]) \
-#                       .withColumn("variant_id", split(col("experiment"), "=")[1])
-
-# # Seleccionar columnas finales ordenadas por usuario y timestamp
-# df_result = df_final.select("user_id", "timestamp", "event_name", "item_id", "site", "experiment_name", "variant_id", "flag_purchase") \
-#                     .orderBy("user_id", "timestamp")
+# Propagar los grupos de compras hacia atrás
+df_delta = df_delta.withColumn(
+    "buy_group",
+    last(when(col("is_buy") == 1, row_number().over(user_window_desc)), ignorenulls=True).over(user_window_desc)
+)
 
 
-# # Guardar la tabla Silver
-# df_result.write.format("delta").mode("overwrite").save(delta_table_silver_path)
-# df_result.filter(col("user_id") == 1683).show(n=1000,truncate=False) #Usuario con usa sola sesion y con compra
-# df_result.filter(col("user_id") == 847572).show(n=1000,truncate=False) #Usuario con varias compras
-# df_result.filter(col("user_id") == 1876).show(n=1000,truncate=False) #Usuario con varias sesiones pero sin compras
+# Asignar grupo 0 a eventos no relacionados con compras
+df_delta = df_delta.withColumn(
+    "flag_purchase",
+    when(col("buy_group").isNull(), lit(False)).otherwise(lit(True))
+)
 
-# print(f"Delta Table guardada en: {delta_table_silver_path}")
+df_delta = df_delta.orderBy("user_id", "timestamp")
+
+# Mostrar resultados
+df_delta.select("user_id", "timestamp", "event_name", "buy_group", "flag_purchase").filter(col("user_id") == 1683).show(n=1000,truncate=False) #Usuario con usa sola sesion y con compra
+df_delta.select("user_id", "timestamp", "event_name", "buy_group", "flag_purchase").filter(col("user_id") == 847572).show(n=1000,truncate=False) #Usuario con varias compras
+df_delta.select("user_id", "timestamp", "event_name", "buy_group", "flag_purchase").filter(col("user_id") == 1876).show(n=1000,truncate=False) #Usuario con varias sesiones pero sin compras
+
+# PARTE II: Explosión de datos después de marcar
+# Limpiar la columna 'experiments' para quitar las llaves '{' y '}'
+df_cleaned = df_delta.withColumn("experiments", regexp_replace(col("experiments"), "[{}]", ""))
+
+# Separar los experimentos en un array
+df_array = df_cleaned.withColumn("experiments_array", split(col("experiments"), ", "))
+
+# Explode para separar cada experimento en filas individuales
+df_exploded = df_array.withColumn("experiment", explode(col("experiments_array")))
+
+# Separar experiment_name y variant_id
+df_final = df_exploded.withColumn("experiment_name", split(col("experiment"), "=")[0]) \
+                      .withColumn("variant_id", split(col("experiment"), "=")[1])
+
+# Seleccionar columnas finales ordenadas por usuario y timestamp
+df_result = df_final.select("user_id", "timestamp", "event_name", "item_id", "site", "experiment_name", "variant_id", "flag_purchase") \
+                    .orderBy("user_id", "timestamp")
+
+
+# Guardar la tabla Silver
+df_result.write.format("delta").mode("overwrite").save(delta_table_silver_path)
+df_result.filter(col("user_id") == 1683).show(n=1000,truncate=False) #Usuario con usa sola sesion y con compra
+df_result.filter(col("user_id") == 847572).show(n=1000,truncate=False) #Usuario con varias compras
+df_result.filter(col("user_id") == 1876).show(n=1000,truncate=False) #Usuario con varias sesiones pero sin compras
+
+print(f"Delta Table guardada en: {delta_table_silver_path}")
 
 
 ########################################
